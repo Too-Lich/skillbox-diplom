bd07872f425504ca9eef051b53541c194870feea
Anatoly Dudkin <serafim30@mail.ru>
2024-02-18 17:47:50 +0300

    append funcs in sms storages

--- internal/storages/sms.go
+++ internal/storages/sms.go
@@ -7,3 +7,14 @@ type SMSStorage []*sms.SMSData
 func (ss *SMSStorage) Add(obj *sms.SMSData) {
 	*ss = append(*ss, obj)
 }
+
+func NewSMSStorage(filename string) (*SMSStorage, error) {
+	return createSMSStorage(filename)
+}
+
+func createSMSStorage(filename string) (*SMSStorage, error) {
+	smsStr, err := pars.FileToString(filename)
+	if err != nil {
+		return nil, err
+	}
+}
